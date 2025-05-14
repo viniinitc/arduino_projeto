42a46e2fc627a32edc722f5d49c6fa3131f83b8f
[viniinitc] <[vinitcavalcantena@gmail.com]>
2025-05-14 12:56:58 -0300

    [codigo praticamente finalizado]

--- palma/palma.ino
+++ palma/palma.ino
@@ -1,6 +1,13 @@
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 
+SoftwareSerial mySoftwareSerial(10, 11);  // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+String line;
+char command;
+int pause = 0;
+int repeat = 0;
+
 const int OUT_PIN = 8;
 long int del; 
 int palma;
@@ -11,14 +18,19 @@ void setup() {
 }
 void loop() {
 
-  Serial.println(digitalRead(OUT_PIN));
+  //Serial.println(digitalRead(OUT_PIN));
   Serial.println(palma);
   if (digitalRead(OUT_PIN) == 1){
     digitalWrite(4, HIGH);
-    palma ++;
-    if ((millis() - del > 500) && millis() - del < 5000) {
-      palma = 0;
+    if (millis() - del > 100){
+      palma += 1;
     }
+    if ((millis() - del > 500)){ 
+      if ((millis() - del < 1500) && millis() - del < 5000) {
+        palma = 0;
+      }
+    }
+    
     del = millis();
   }
   else{
@@ -26,11 +38,11 @@ void loop() {
       digitalWrite(4, LOW);
     }
   }
-  
-  
-
   if (millis() - del > 5000){
     palma = 0;
     del = millis();
   }
+  //if (palma == 10){
+    //myDFPlayer.play(1);
+  //}
 }
