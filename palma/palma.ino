c0a0398301e2f22aae4b62b0775f644ea2759f44
[viniinitc] <[vinitcavalcantena@gmail.com]>
2025-05-14 12:08:10 -0300

    [palma] agr se bater palma mt rapido ele reseta (eu acho)

--- palma/palma.ino
+++ palma/palma.ino
@@ -1,5 +1,9 @@
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
 const int OUT_PIN = 8;
 long int del; 
+int palma;
 void setup() {
   del = millis();
   Serial.begin(9600);
@@ -8,14 +12,25 @@ void setup() {
 void loop() {
 
   Serial.println(digitalRead(OUT_PIN));
+  Serial.println(palma);
   if (digitalRead(OUT_PIN) == 1){
     digitalWrite(4, HIGH);
+    palma ++;
+    if ((millis() - del > 500) && millis() - del < 5000) {
+      palma = 0;
+    }
     del = millis();
   }
   else{
-    if ((millis() - del) > 1000){
+    if ((millis() - del) > 500){
       digitalWrite(4, LOW);
-      del = millis();
     }
+  }
+  
+  
+
+  if (millis() - del > 5000){
+    palma = 0;
+    del = millis();
   }
 }
